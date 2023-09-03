8ce75fb50e41f0fc19286d852bcbf78067e23124
Hariharan <138101854+Devharirolls@users.noreply.github.com>
2023-09-03 21:26:44 +0530

    Update fetchFromAPI.js

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -24,30 +24,3 @@ export const fetchFromAPI = async (url, queryParams = {}) => {
   }
 };
 
-
-
-// import axios from "axios";
-
-// const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
-
-// const options = {
-//     url: BASE_URL,
-//     params: {
-//       maxResults: '50'
-//     },
-//     headers: {
-//       'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-//       'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
-//     }
-//   };
-
-
-
-//   export const fetchFromAPI = async (url) => {
-//     try {
-//       const { data } = await axios.get(`${BASE_URL}/${url}`, options);
-//       return data;
-//     } catch (e) {
-//       console.log("error", e);
-//     }
-//   };
